32ab27cc7eafe71f013a8435ebee1d655b7b5064
jfrangop <>
2023-02-04 19:24:50 +0100

    update user_questioner

--- user_questioner.py
+++ user_questioner.py
@@ -1,5 +1,6 @@
 # User questioner
-def get_user_input(prompt):
-    # code to get user input
-    user_input = input(prompt)
+def get_user_input(user_ouput):
+    print("----")
+    print(user_output)
+    user_input = input("-> ")
     return user_input
