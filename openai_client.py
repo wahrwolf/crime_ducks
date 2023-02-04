691449dc0b352ff8f9e25c1e85c63167d586b7e5
Vincent Dahmen <wahrwolf@wolfpit.net>
2023-02-04 20:08:09 +0100

    openai_client: adds option to specify response length with fallback to config

--- openai_client.py
+++ openai_client.py
@@ -3,16 +3,19 @@ import toml
 
 config = toml.load("config.toml")
 with open(config["openai"]["secret_file"]) as f:
-    openai.api_key = f.read()
+    openai.api_key = f.read().splitlines()[0]
+
 
 # OpenAI client
-def get_response(prompt):
+def get_response(prompt, tokens=None):
     response = openai.Completion.create(
         engine=config["openai"]["model"],
         prompt=prompt,
-        max_tokens=config["openai"].get("max_tokens", 2048),
+        max_tokens=tokens if tokens else config["openai"].get("max_tokens", 2048),
         n=1,
         stop=None,
         temperature=config["openai"].get("temperature", 0.7)
     ).choices[0].text
     return response
+
+print(get_response("This is a test, if I can speak to the API. Please tell me a cute animal joke"))
