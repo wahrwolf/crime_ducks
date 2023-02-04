52ee50fc3656979971acb177ba5c624bf37ce24a
Vincent Dahmen <vincent.dahmen@gmail.com>
2023-02-04 18:43:16 +0100

    Create skeleton.py

---
+++ skeleton.py
@@ -0,0 +1,56 @@
+import openai
+
+# Game world builder
+def build_game_world():
+    # code to generate initial gamestate (state 0)
+    gamestate = {...}
+    return gamestate
+
+# Prompt generator
+def generate_prompt(gamestate):
+    # code to generate prompt based on gamestate
+    prompt = ...
+    return prompt
+
+# User questioner
+def get_user_input(prompt):
+    # code to get user input
+    user_input = input(prompt)
+    return user_input
+
+# OpenAI client
+def openai_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    ).choices[0].text
+    return response
+
+def play_game():
+    # Initialize game world
+    gamestate = build_game_world()
+    
+    # Play the game
+    while True:
+        # Generate prompt
+        prompt = generate_prompt(gamestate)
+        
+        # Get user input
+        user_input = get_user_input(prompt)
+        
+        # Update gamestate based on user input
+        gamestate = update_gamestate(gamestate, user_input)
+        
+        # Get response from OpenAI
+        response = openai_response(prompt)
+        
+        # Update gamestate based on OpenAI response
+        gamestate = update_gamestate(gamestate, response)
+
+# Start the game
+if __name__ == "__main__":
+    play_game()
